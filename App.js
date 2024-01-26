2969de02b485c1a7d5879d037f2edcd545a6a5c4
Yukendhira <71933787+Yukendhira@users.noreply.github.com>
2024-01-26 22:34:15 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+// App.js
+
+import React from 'react';
+import BmiCalculator from './Components/BmiCalculator';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Your React App</h1>
+      <BmiCalculator />
+      {/* Add other components as needed */}
+    </div>
+  );
+};
+
+export default App;
